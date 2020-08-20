5c033dc7dc9fb47430153aeff2616cf555b0c8db
Azam Kamal <azam.kamal3@gmail.com>
2020-08-20 17:08:49 +0500

    cleaned

---
+++ cleann.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+#import seaborn as sns
+#import matplotlib.pyplot as plt
+#from sklearn.cluster import KMeans
+#from skleran.preprocessing import StandardScaler
+#from sklearn.decomposition import PCA
+#import prince
+
+df=pd.read_csv("surgical_intruments_pakistan.csv")
+print(df.shape)
